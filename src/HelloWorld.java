d114221e7d84c1d643356260b2341afdfe372e91
Jakub Lukšík <jakub.luksik@infis.cz>
2024-09-12 15:12:24 +0200

    commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("|\\---/|");
+        System.out.println("| o_o |");
+        System.out.println(" _^_/ ");
+    }
+
+}
