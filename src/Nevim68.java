6ea382259cab16fa2e2c5e129d7e774a58d3e975
jakub.luksik <jakub.luksi@infis.cz>
2024-10-21 13:14:54 +0200

    commit

--- src/Nevim68.java
+++
@@ -1,5 +0,0 @@
-public class Nevim68 {
-    public static void main(String[] args) {
-        System.out.println("jjjj");
-    }
-}
--- src/Nevim69.java
+++
@@ -1,5 +0,0 @@
-public class Nevim69 {
-    public static void main(String[] args) {
-        System.out.println("hhhhh");
-    }
-}
