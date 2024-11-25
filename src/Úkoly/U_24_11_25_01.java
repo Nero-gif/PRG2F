6dc7a7983403ec383a6fc1b55f747299caff3135
jakub.luksik <jakub.luksi@infis.cz>
2024-11-25 13:19:01 +0100

    commit

--- src/Úkoly/U_24_11_25_01.java
+++ src/Úkoly/U_24_11_25_01.java
@@ -1,7 +1,28 @@
 package Úkoly;
 
+import java.util.Scanner;
+
 public class U_24_11_25_01 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Zadejte jméno.");
+        String jmeno = sc.next();
+        System.out.println("Zadejte přijímení");
+        String prijimeni = sc.next();
+
+        String out;
+
+        out = String.valueOf(jmeno.charAt(2));
+
+        for (int i = jmeno.length(); i > 0; i--) {
+            out = out + jmeno.valueOf(i);
+        }
+
+        System.out.println(out);
+        System.out.println(jmeno+prijimeni);
+
+
 
     }
 }
