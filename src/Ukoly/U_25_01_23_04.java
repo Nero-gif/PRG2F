09c75f146f2118aa8e534870a81d863eea1e782d
Jakub Lukšík <jakub.luksik@infis.cz>
2025-01-23 15:25:12 +0100

    commit

---
+++ src/Ukoly/U_25_01_23_04.java
@@ -0,0 +1,23 @@
+package Ukoly;
+
+import java.util.Scanner;
+
+public class U_25_01_23_04 {
+
+    static void sumOfDigits(int n){
+        for (int i = 0; i < n.length; i++) {
+
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Zadejte číslo.");
+        int n = sc.nextInt();
+        n = 545454545;
+        int csoucet;
+        sumOfDigits(n);
+
+        System.out.println("Ciferný součet Vašeho čísla je "+csoucet);
+    }
+}
