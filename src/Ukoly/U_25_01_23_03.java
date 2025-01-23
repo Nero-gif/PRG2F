9aac0da636bba76011197a74f126cff4d4b4e918
Jakub Lukšík <jakub.luksik@infis.cz>
2025-01-23 15:15:42 +0100

    commit

---
+++ src/Ukoly/U_25_01_23_03.java
@@ -0,0 +1,31 @@
+package Ukoly;
+
+import java.util.Scanner;
+
+public class U_25_01_23_03 {
+    static void validateNick(String nick, String jmeno, String prij){
+        String idk = String.valueOf(123456789);
+        jmeno = jmeno + idk;
+        prij = prij +idk;
+        for (int i = 0; i < 3; i++) {
+            nick = nick + jmeno.charAt(i);
+        }
+        for (int i = 0; i < 3; i++) {
+            nick = nick + prij.charAt(i);
+        }
+        System.out.println("Doporučený nick je "+nick);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Zadejte jméno.");
+        String jmeno = sc.next();
+        System.out.println("Zadejte přijímení");
+        String prij = sc.next();
+
+        String nick = "";
+
+        validateNick(nick, jmeno, prij);
+    }
+
+}
