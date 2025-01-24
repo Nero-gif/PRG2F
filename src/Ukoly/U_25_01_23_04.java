79af86d2dd3b8c9af93535de1bc449fc1c77a4f2
Jakub Lukšík <jakub.luksik@infis.cz>
2025-01-24 14:09:51 +0100

    commit

--- src/Ukoly/U_25_01_23_04.java
+++ src/Ukoly/U_25_01_23_04.java
@@ -4,20 +4,26 @@ import java.util.Scanner;
 
 public class U_25_01_23_04 {
 
-    static void sumOfDigits(int n){
-        for (int i = 0; i < n.length; i++) {
+    public static int sumOfDigits(int n) {
+        n = Math.abs(n);
 
+        // Základní podmínka rekurze: jednociferné číslo
+        if (n < 10) {
+            return n;
+        } else {
+            // Součet poslední číslice a součet zbývající části
+            return (n % 10) + sumOfDigits(n / 10);
         }
     }
+
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        // Načtení čísla od uživatele
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Zadejte číslo: ");
+        int number = scanner.nextInt();
 
-        System.out.println("Zadejte číslo.");
-        int n = sc.nextInt();
-        n = 545454545;
-        int csoucet;
-        sumOfDigits(n);
-
-        System.out.println("Ciferný součet Vašeho čísla je "+csoucet);
+        // Výpočet a výpis výsledku
+        int result = sumOfDigits(number);
+        System.out.println("Součet číslic čísla " + number + " je: " + result);
     }
 }
